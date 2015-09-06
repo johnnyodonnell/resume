0ca91153f1b7e6932ba426ae4df9784c922d2acc
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2015-09-06 13:39:36 -0900

    Resume added to rails app

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Resume::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'static_pages#resume'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
